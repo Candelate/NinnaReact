9709b9091ee068edb3a8810887174e704abe9415
NeriCardozo <75796427+NeriCardozo@users.noreply.github.com>
2021-10-18 21:27:27 -0300

    Agregando card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Product from './components/Product.js'
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>NinnaByAc</h1>
+      <Product />
     </div>
   );
 }
