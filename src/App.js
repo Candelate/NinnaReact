a5b15a2e732137856b73fd8d1ed741cbec39b5d9
CandelaTe <candeetevez@gmail.com>
2021-10-23 23:44:16 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Item from './components/Item'
 import ItemList from './components/ItemList';
 import CheckoutCard from './components/CheckoutCard';
 import CheckoutPage from './components/CheckoutPage';
+import { Footer } from './components/Footer';
 
 
 
@@ -16,27 +17,7 @@ function App() {
       <NavBar />
       <CheckoutCard />
       <CheckoutPage />
-
-      <div>
-          <footer class="footerPago">
-                <div>
-                  <h3 class="pagos">Pagos con todas la tarjetas de credito</h3> 
-              </div>
-              <div class="logoTarjetasContainer">
-                <img src="./logosTarjetas/cabal.png" alt=""/>
-                <img src="./logosTarjetas/american-express.png" alt=""/>
-                <img src="./logosTarjetas/link.png" alt=""/>
-                <img src="./logosTarjetas/naranja.png" alt=""/>
-                <img src="./logosTarjetas/nativa.png" alt=""/>
-                <img src="./logosTarjetas/mastercard.jpg" alt=""/>
-              </div>
-              <div class="contacto">
-                <p>Pordes contactarte al 1155667788 ante cualquier duda de Lunes a Viernes de 10 a 17hs.</p>
-              </div>  
-          </footer>
-      </div>
-
-
+      <Footer />
     </div>
   );
 }
